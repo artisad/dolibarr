6d69b4118c2764ad7af15bfcc96c29d563eee3d9
Vaadasch <qqqrno@gmail.com>
2024-04-27 19:39:30 +0200

    Create config-dictionaries.php

---
+++ scripts-init/config-dictionaries.php
@@ -0,0 +1,54 @@
+<?php
+
+require_once '../htdocs/master.inc.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
+require_once DOL_DOCUMENT_ROOT.'/core/class/ccountry.class.php';
+require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
+
+/*
+require_once DOL_DOCUMENT_ROOT.'/core/db/DoliDB.class.php';
+$db = new DoliDB();
+$db->connect();
+*/
+
+$sql = "SELECT table_name
+FROM information_schema.columns 
+WHERE table_schema = '".$dolibarr_main_db_name."'
+  AND table_name LIKE '".$dolibarr_main_db_prefix."c_%'
+  AND column_name IN ('fk_pays', 'active')
+GROUP BY table_name
+HAVING COUNT(DISTINCT column_name) = 2;";
+
+$result = $db->query($sql);
+if ($result)
+{
+	$num = $db->num_rows($result);
+	$i = 0;
+	while ($i < $num)
+	{
+    $obj = $db->fetch_object($result);
+		$tablename = $obj->table_name;
+
+    // ALTER table to default value for active at 0
+    $sql = "ALTER TABLE ".$table_name." MODIFY COLUMN active TINYINT(1) DEFAULT 0";
+    $db->query($sql);
+
+    // Pass all active at 0
+    $sql = "UPDATE $table_name SET active = 0 WHERE 1";
+    $db->query($sql);
+
+    // Pass all values of current country to active at 1
+    $sql = "UPDATE $table_name SET active = 1 WHERE fk_pays = ".$mysoc->country_id;
+    $db->query($sql);
+		$i++;
+	}
+}
+
+$db->commit();
+
+
+?>
+
+
+
+    
