7c2d83ed074aaba2e7b57169aaf75a845ea816f0
Vaadasch <qqqrno@gmail.com>
2024-04-28 13:27:57 +0200

    Update rename_forme_juridique.sql

--- scripts-init/sql/rename_forme_juridique.sql
+++ scripts-init/sql/rename_forme_juridique.sql
@@ -1,9 +1,9 @@
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Entrepreneur individuel (EI)' WHERE `llx_c_forme_juridique`.`rowid` = 52;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Personne morale de droit étranger, immatriculée au RCS' WHERE `llx_c_forme_juridique`.`rowid` = 60;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Personne morale de droit étranger, non immatriculée au RCS' WHERE `llx_c_forme_juridique`.`rowid` = 61;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Société coopérative (SCOP)' WHERE `llx_c_forme_juridique`.`rowid` = 63;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Société en nom collectif (SNC)' WHERE `llx_c_forme_juridique`.`rowid` = 64;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Société en commandite (SCA/SCS)' WHERE `llx_c_forme_juridique`.`rowid` = 65;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Société à responsabilité limitée (SARL)' WHERE `llx_c_forme_juridique`.`rowid` = 66;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Société anonyme (SA)' WHERE `llx_c_forme_juridique`.`rowid` = 68;
-UPDATE `llx_c_forme_juridique` SET libelle` = 'Société par actions simplifiée (SAS)' WHERE `llx_c_forme_juridique`.`rowid` = 69;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Entrepreneur individuel (EI)' WHERE `llx_c_forme_juridique`.`rowid` = 52;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Personne morale de droit étranger, immatriculée au RCS' WHERE `llx_c_forme_juridique`.`rowid` = 60;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Personne morale de droit étranger, non immatriculée au RCS' WHERE `llx_c_forme_juridique`.`rowid` = 61;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Société coopérative (SCOP)' WHERE `llx_c_forme_juridique`.`rowid` = 63;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Société en nom collectif (SNC)' WHERE `llx_c_forme_juridique`.`rowid` = 64;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Société en commandite (SCA/SCS)' WHERE `llx_c_forme_juridique`.`rowid` = 65;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Société à responsabilité limitée (SARL)' WHERE `llx_c_forme_juridique`.`rowid` = 66;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Société anonyme (SA)' WHERE `llx_c_forme_juridique`.`rowid` = 68;
+UPDATE `llx_c_forme_juridique` SET `libelle` = 'Société par actions simplifiée (SAS)' WHERE `llx_c_forme_juridique`.`rowid` = 69;
